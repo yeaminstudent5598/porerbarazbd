b79e2ea8554d44efd2b1b34351a8023392215f54
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2025-10-27 15:59:22 +0600

    Update: modules/category/category.validation.ts

--- modules/category/category.validation.ts
+++ modules/category/category.validation.ts
@@ -3,8 +3,11 @@ import { z } from 'zod';
 
 const categoryFormSchema = z.object({
   name: z.string().min(2, 'Name must be at least 2 characters'),
-  slug: z.string().min(2).optional().or(z.literal(''))
-          .transform(val => val ? val.toLowerCase().replace(/ /g, '-') : undefined),
+  slug: z.string()
+    .min(2)
+    .optional()
+    .or(z.literal(''))
+    .transform(val => val ? val.toLowerCase().replace(/ /g, '-') : undefined),
 });
 
 const updateCategoryFormSchema = categoryFormSchema.partial();
