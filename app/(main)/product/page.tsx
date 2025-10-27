cb7e1e9cb2f0a95c2da223ecfc8b7f8bed6cccbb
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2025-10-27 15:59:15 +0600

    Update: app/(main)/product/page.tsx

---
+++ app/(main)/product/page.tsx
@@ -0,0 +1,13 @@
+// app/(main)/product/page.tsx
+import React from 'react';
+
+const ProductPage = () => {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>Products</h1>
+      <p>Here you can browse all products.</p>
+    </div>
+  );
+};
+
+export default ProductPage;
