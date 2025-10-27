7969487099f946efcc7e63c8566887d78ae48539
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2025-10-27 15:59:14 +0600

    Update: app/(main)/cart/page.tsx

---
+++ app/(main)/cart/page.tsx
@@ -0,0 +1,13 @@
+// app/(main)/cart/page.tsx
+import React from 'react';
+
+const CartPage = () => {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>Your Cart</h1>
+      <p>Here you can see all your products in the cart.</p>
+    </div>
+  );
+};
+
+export default CartPage;
