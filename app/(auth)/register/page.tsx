0a53dff2f4f6b2362b2213ea2042751222209875
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2025-10-27 15:59:13 +0600

    Update: app/(auth)/register/page.tsx

---
+++ app/(auth)/register/page.tsx
@@ -0,0 +1,13 @@
+// app/(user)/dashboard/profile/page.tsx
+import React from 'react';
+
+const ProfilePage = () => {
+  return (
+    <div>
+      <h1>User Profile</h1>
+      <p>This is your profile page.</p>
+    </div>
+  );
+};
+
+export default ProfilePage;
