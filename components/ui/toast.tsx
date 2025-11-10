8e649b8b7d62887aafa576ed2a6ef3717940ec37
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2025-11-10 12:36:04 +0600

    Update: components/ui/toast.tsx

---
+++ components/ui/toast.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import * as React from "react";
+import * as ToastPrimitives from "@radix-ui/react-toast";
+import { cn } from "@/app/lib/utils";
+
+const ToastProvider = ToastPrimitives.Provider;
+
+const ToastViewport = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>((props, ref) => (
+  <ToastPrimitives.Viewport
+    ref={ref}
+    className={cn(
+      "fixed bottom-0 right-0 flex flex-col p-4 gap-2 w-[390px] max-w-full m-0 list-none z-[999]",
+      props.className
+    )}
+    {...props}
+  />
+));
+ToastViewport.displayName = "ToastViewport";
+
+const Toast = React.forwardRef<
+  React.ElementRef<typeof ToastPrimitives.Root>,
+  React.ComponentProps<typeof ToastPrimitives.Root>
+>((props, ref) => (
+  <ToastPrimitives.Root
+    ref={ref}
+    className={cn(
+      "bg-white border rounded-md p-4 shadow-md grid gap-2",
+      props.className
+    )}
+    {...props}
+  />
+));
+Toast.displayName = "Toast";
+
+const ToastTitle = React.forwardRef<
+  React.ElementRef<typeof ToastPrimitives.Title>,
+  React.ComponentProps<typeof ToastPrimitives.Title>
+>((props, ref) => (
+  <ToastPrimitives.Title ref={ref} className="font-semibold text-sm" {...props} />
+));
+ToastTitle.displayName = "ToastTitle";
+
+const ToastDescription = React.forwardRef<
+  React.ElementRef<typeof ToastPrimitives.Description>,
+  React.ComponentProps<typeof ToastPrimitives.Description>
+>((props, ref) => (
+  <ToastPrimitives.Description
+    ref={ref}
+    className="text-sm text-muted-foreground"
+    {...props}
+  />
+));
+ToastDescription.displayName = "ToastDescription";
+
+const ToastAction = ToastPrimitives.Action;
+
+export { Toast, ToastProvider, ToastViewport, ToastTitle, ToastDescription, ToastAction };
