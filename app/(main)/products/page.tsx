f1921b802d521913c6c44d5b156d452fced0cb30
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2025-11-15 17:46:29 +0600

    Update: app/(main)/products/page.tsx

--- app/(main)/products/page.tsx
+++ app/(main)/products/page.tsx
@@ -23,13 +23,14 @@ import { ProductCard } from '@/app/components/shared/ProductCard';
 
 // Demo Data (Replace with fetching logic)
 const allProducts = [
-  { id: 1, name: "কালোজিরা আচার", imageUrl: "https://efoodis.com/public/uploads/product/1756417513-%E0%A6%95%E0%A6%BE%E0%A6%B2%E0%A7%8B%E0%A6%9C%E0%A6%BF%E0%A6%B0%E0%A6%BE-%E0%A6%AE%E0%A6%BF%E0%A6%95%E0%A7%8D%E0%A6%B8-%E0%A6%86%E0%A6%9A%E0%A6%BE%E0%A6%B0.jpg", oldPrice: 1250, newPrice: 890, discount: "29%", category: "Achar" },
-  { id: 2, name: "ঝুড়ি পিঠা", imageUrl: "https://efoodis.com/public/uploads/product/1754690259-juri.webp", oldPrice: 850, newPrice: 790, discount: "7%", category: "Pitha" },
-  { id: 3, name: "রসুনের আচার", imageUrl: "https://efoodis.com/public/uploads/product/1754690259-juri.webp", oldPrice: 850, newPrice: 690, discount: "19%", category: "Achar" },
-  { id: 4, name: "১ কিজিঃ ৪ পিঠা কম্বো", imageUrl: "https://efoodis.com/public/uploads/product/1758055563-combo-offer.jpg", oldPrice: 990, newPrice: 690, discount: "30%", category: "Pitha" },
-  { id: 5, name: "ঝিনুক পিঠা", imageUrl: "https://efoodis.com/public/uploads/product/1756499233-jhinuk-pitha.jpg", oldPrice: 850, newPrice: 590, discount: "31%", category: "Pitha" },
-  { id: 7, name: "১.৫ লিটার ৪ স্পেশাল কম্বো", imageUrl: "https://efoodis.com/public/uploads/product/1758059095-combo-web-efoodis.jpg", oldPrice: 1800, newPrice: 1250, discount: "31%", category: "Oil" },
-  { id: 12, name: "অর্গানিক সরিষার তেল", imageUrl: "https://efoodis.com/public/uploads/category/1736429963-organic.webp", oldPrice: 500, newPrice: 450, discount: "10%", category: "Oil" },
+      { id: 1, name: "কালোজিরা আচার", imageUrl: "/Images/pp1.jpeg", oldPrice: 1250, newPrice: 890, discount: "29%", category: "Achar" },
+    { id: 2, name: "ঝুড়ি পিঠা", imageUrl: "/Images/p5.jpeg", oldPrice: 850, newPrice: 790, discount: "7%", category: "Pitha" },
+    { id: 3, name: "রসুনের আচার", imageUrl: "/Images/pp2.jpeg", oldPrice: 850, newPrice: 690, discount: "19%", category: "Achar" },
+    { id: 4, name: "১ কিজিঃ ৪ পিঠা কম্বো অফার", imageUrl: "/Images/pp3.jpeg", oldPrice: 990, newPrice: 690, discount: "30%", category: "Pitha" },
+    { id: 5, name: "ঝিনুক পিঠা", imageUrl: "/Images/p3.jpeg", oldPrice: 850, newPrice: 590, discount: "31%", category: "Pitha" },
+    { id: 6, name: "নকশি পিঠা", imageUrl: "/Images/p1.jpeg", oldPrice: 850, newPrice: 590, discount: "31%", category: "Pitha" },
+    { id: 7, name: "১.৫ লিটার ৪ স্পেশাল কম্বো", imageUrl: "/Images/pp4.jpeg", oldPrice: 1800, newPrice: 1250, discount: "31%", category: "Oil" },
+    { id: 8, name: "নকশি পিঠা", imageUrl: "/Images/p6.jpeg", oldPrice: 850, newPrice: 690, discount: "19%", category: "Pitha" },
   // Add more products...
 ];
 
