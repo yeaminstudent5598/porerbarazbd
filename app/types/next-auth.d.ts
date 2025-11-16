8b7df51e4c50926f5640fef9ba8d5e94ef07442e
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2025-11-16 15:24:31 +0600

    Update: app/types/next-auth.d.ts

--- app/types/next-auth.d.ts
+++ app/types/next-auth.d.ts
@@ -1,25 +1,25 @@
-// types/next-auth.d.ts
+// app/types/next-auth.d.ts
 import NextAuth, { DefaultSession, DefaultUser } from "next-auth";
 
 declare module "next-auth" {
   interface Session {
     user: {
       id: string;
-      name?: string | null;
-      email?: string | null;
-      image?: string | null;
-    };
+      role: string;
+    } & DefaultSession["user"]; // keeps name, email, image
     accessToken?: string;
   }
 
   interface User extends DefaultUser {
     id: string;
+    role: string;
   }
 }
 
 declare module "next-auth/jwt" {
   interface JWT {
     id: string;
-    accessToken?: string;
+    role: string;
+    customAccessToken?: string;
   }
 }
